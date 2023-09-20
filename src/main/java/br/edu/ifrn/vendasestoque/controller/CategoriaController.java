7248a466e41d53cf72c74c1b6ec7b5a82bfdb8d3
Cecília do Nascimento Miranda <20221148060012@ifrn.local>
2023-09-20 10:27:23 -0300

    feat: implementação métodos add remove e atualizar

--- src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
+++ src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
@@ -1,14 +1,27 @@
 package br.edu.ifrn.vendasestoque.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableArgumentResolver;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.util.List;
+
 
 import br.edu.ifrn.vendasestoque.domain.categoria.Categoria;
 import br.edu.ifrn.vendasestoque.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("categorias")
@@ -18,7 +31,36 @@ public class CategoriaController {
     private CategoriaRepository repository;
 
     @PostMapping
-    public ResponseEntity<Categoria> cadastrarCategoria(@RequestBody Categoria categoria){
-        return ResponseEntity.ok(repository.save(categoria));
+    public ResponseEntity cadastrarCategoria(@RequestBody Categoria categoria, UriComponentsBuilder uriBuilder){
+        Categoria categoriaLocal = repository.save(categoria);
+        var uri = uriBuilder.path("/categorias/{id}").buildAndExpand(categoriaLocal.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var  categoria = repository.findById(id);
+        return ResponseEntity.ok(categoria);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Categoria>> listar(@PageableDefault(size = 4, sort = {"nome"}) Pageable paginacao){
+        var categorias = repository.findAll(paginacao);
+        return ResponseEntity.ok(categorias);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id){
+        var categoria = repository.getReferenceById(id);
+        repository.delete(categoria);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity atualizar(@RequestBody Categoria categoria){
+        var categoriaLocal = repository.getReferenceById(categoria.getId());
+        repository.save(categoriaLocal);
+        return ResponseEntity.ok(categoriaLocal);
     }
 }
--- src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
+++ src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
@@ -1,10 +1,14 @@
 package br.edu.ifrn.vendasestoque.domain.categoria;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,10 +20,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Categoria {
+public class Categoria implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    
     private String nome;
 }
